ad84ce881d01c58e46dda649437a99626c5e0b5b
00016560 <wiut00016560@gmail.com>
2024-03-14 11:03:03 +0500

    step21

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express')
-
 const body_parser = require('body-parser')
 
 const path = require('path')
-
-
 global.mock_db = path.join(__dirname, './data/mock_db.json');
 
 const web_route = require('./routes/web/home')
@@ -16,12 +13,11 @@ app.set('view engine', 'pug');
 
 app.use('/css', express.static('public/css'))
 app.use('/js', express.static('public/js'))
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', api_route); 
-app.use('/', web_route); 
+app.use('./', web_route); 
 
 app.use((req, res) => {
     res.redirect('/');
