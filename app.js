3531ed0f84752168d6016d1f33b2731196d50e51
00016560 <wiut00016560@gmail.com>
2024-03-14 13:43:25 +0500

    step32

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ app.use((req, res) => {
     res.redirect('/');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
